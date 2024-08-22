83b32fdcddd23599c1253938e55601f10df05960
Kevin Wißmer <74767313+KevinWissmer@users.noreply.github.com>
2024-08-22 17:51:21 +0200

    style added

---
+++ template.js
@@ -0,0 +1,14 @@
+function getWeatherCardTemplate(){
+    return `
+        <div class="weather-card">
+            <div class="weather-date">22. August 2024</div>
+                <div class="weather-info">
+                    <div class="temp-range">
+                        <span class="temp-low">Low: 15°C</span>
+                        <span class="temp-high">High: 25°C</span>
+                    </div>
+                <div class="wind-speed">Max Wind: 20 km/h</div>
+            </div>
+        </div>
+    `
+}
